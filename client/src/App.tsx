b358c9148ccad53eff2328fff6145b8948085b4a
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-12-23 21:07:30 +0700

    delete(client): delete component and redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,7 @@
 import React from "react";
-import { RouterProvider, Route, createBrowserRouter } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Navbar />
-        <Dashboard />
-      </>
-    ),
-  },
-  {
-    path: "/login",
-    element: (
-      <>
-        <Navbar />
-        <Login />
-      </>
-    ),
-  },
-  {
-    path: "/register",
-    element: (
-      <>
-        <Navbar />
-        <Register />
-      </>
-    ),
-  },
-]);
 
 const App: React.FC = () => {
-  return <RouterProvider router={router} />;
+  return <div>Goal Beyond</div>;
 };
 
 export default App;
