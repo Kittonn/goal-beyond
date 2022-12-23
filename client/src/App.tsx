d526f0a108d13993c1eb91f73e5e4ad59b864613
Kittipod Lambangchang <tonkittipod@gmail.com>
2022-12-23 17:14:51 +0700

    feat(client): add windi.config.ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,36 @@ import { RouterProvider, Route, createBrowserRouter } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Dashboard /> },
-  { path: "/login", element: <Login /> },
-  { path: "/register", element: <Register /> },
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <Dashboard />
+      </>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <>
+        <Navbar />
+        <Login />
+      </>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <>
+        <Navbar />
+        <Register />
+      </>
+    ),
+  },
 ]);
 
 const App: React.FC = () => {
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,7 +1,16 @@
 import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "../store/store";
+import { useNavigate } from "react-router-dom";
+import { getGoals, reset } from "../store/goal.slice";
+import Loading from "../components/Loading";
 
 const Dashboard: React.FC = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
 };
 
 export default Dashboard;
